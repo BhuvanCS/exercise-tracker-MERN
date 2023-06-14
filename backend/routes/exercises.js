d9fa4ba0ffc14f729c72c0104a7793278f50b4e9
Bhuvan C S <205bhuvics@gmail.com>
2023-06-14 21:12:58 +0530

    Add API Endpoints - 2(Exercise)

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -19,4 +19,26 @@ router.route('/add').post((req, res) => {
     .catch((err) => {return res.status(400).json('Error: '+ err)})
 })
 
+router.route('/:id')
+    .get((req, res) => {
+        Exercise.findById(req.params.id)
+        .then((exercise) => {return res.json(exercise)})
+        .catch((err) => {return res.status(400).json('Error' + err.message)})
+    })
+    .delete((req, res) => {
+        Exercise.findByIdAndDelete(req.params.id)
+        .then(() => {return res.json("Excercise deleted successfully!")})
+        .catch((err) => {return res.status(400).json('Error' + err.message)})
+    })
+
+router.route('/:id').patch((req, res) => {
+    Exercise.findByIdAndUpdate(
+        req.params.id,
+        {username: req.body.username, description: req.body.description, duration: Number(req.body.duration), date: (req.body.date)},
+        {new: true}
+    )
+    .then(() => {return res.json("Succesfully updated!")})
+    .catch(err => {return res.status(400).json('Error: ' + err.message)})
+})
+
 module.exports = router;
