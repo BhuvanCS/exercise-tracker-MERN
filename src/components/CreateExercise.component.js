6909532b6a0fbcf76d332ef186293c7d2c8eb722
Bhuvan C S <205bhuvics@gmail.com>
2023-06-17 18:29:42 +0530

    Part1 - Create Exercise Log (Unf)

--- src/components/CreateExercise.component.js
+++ src/components/CreateExercise.component.js
@@ -1,10 +1,73 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
 
-function CreateExercise(){
-    return <div>
-    <h1>Hello</h1>
-        <p> You are on Exercise Create page</p>
-    </div>
+function CreateExercise(props){
+    const [currUser, setCurrUser] = React.useState({
+        username: "",
+        description: "",
+        duration: 0,
+        date: new Date(),
+        users: []
+    })
+
+    //Lifecycle method which is called everytime page is loaded
+    React.useEffect(() => {
+        setCurrUser((prev) => {
+            return {
+                ...prev,
+                users: ['Test User A', 'Test User B'],
+                username: 'Test User A'
+            }
+        })
+    }, [])
+
+    function onChange(event){
+        setCurrUser((prev) => {
+            return {
+                ...prev,
+                [event.target.name]: event.target.value
+            }
+
+        })
+    }
+
+    function onSubmit(event) {
+        event.preventDefault();
+        const exercise = {
+            username: currUser.username,
+            description: currUser.description,
+            duration: currUser.duration,
+            date: currUser.date
+        }
+        console.log(exercise);
+    }
+    return (
+        <div>
+            <Form onSubmit = {onSubmit}>
+            <Form.Select aria-label="Default select example">
+                {
+                    currUser.users.map(function (user) {
+                        return (
+                            <option value={user}>{user}</option>
+                        )
+                    })
+                }
+            </Form.Select>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                    <Form.Label>Desription</Form.Label>
+                    <Form.Control onChange = {onChange} as="textarea" rows={2} />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                    <Form.Label>Duration in mins</Form.Label>
+                    <Form.Control onChange = {onChange} type="text" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                    <Form.Label>Date</Form.Label>
+                    <Form.Control onChange = {onChange} type="text" />
+                </Form.Group>
+            </Form>
+        </div>
+    )
 }
 
 export default CreateExercise;
